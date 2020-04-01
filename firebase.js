c5101fb0c1ea9f95164ff07bdb704b71c44a369d
Mooffyman13 <n.a.smith@student.saintkentigern.com>
2020-04-01 15:38:14 +1300

    2 files modified, 1 added

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDy_Lt5LQgETr3VXpIgBDY1B3vVKKjjkjY",
+    authDomain: "reminder-app-2fde8.firebaseapp.com",
+    databaseURL: "https://reminder-app-2fde8.firebaseio.com",
+    projectId: "reminder-app-2fde8",
+    storageBucket: "reminder-app-2fde8.appspot.com",
+    messagingSenderId: "564581288421",
+    appId: "1:564581288421:web:f63b7a9bd86fef8427070c",
+    measurementId: "G-RKCBXV9ES9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  export default firebase;
