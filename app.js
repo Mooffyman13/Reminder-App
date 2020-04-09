f76c251413757c6280276cf11b9fbff84ed07120
Mooffyman13 <n.a.smith@student.saintkentigern.com>
2020-04-09 14:32:43 +1200

    2 files modified

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import home from './components/home';
-import reminderinput from './components/reminderinput';
-import stuff from './components/stuff';
-import error from './components/error';
-import Navigation from './components/navigation';
+import home from './components/home.js';
+import reminderinput from './components/reminderinput.js';
+import stuff from './components/stuff.js';
+import error from './components/error.js';
+import Navigation from './components/navigation.js';
 import logo from './logo.svg';
 import 'style.css';
 import firebase from './firebase.js';
--- components/navigation.js
+++ components/navigation.js
@@ -5,9 +5,9 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
     return (
        <div className="navbar">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">Create A Reminder</NavLink>
-          <NavLink to="/contact">Stuff</NavLink>
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/reminderinput">Create A Reminder</NavLink>
+          <NavLink to="/stuff">Stuff</NavLink>
        </div>
     );
 }
