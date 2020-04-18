7af856e99a45ae791547d5572d7c599ff51fa689
Mooffyman13 <n.a.smith@student.saintkentigern.com>
2020-04-18 20:46:14 +1200

    1 file modified

--- components/home.js
+++ components/home.js
@@ -7,16 +7,25 @@ import firebase from './firebase.js';
 
 class App extends Component {
   constructor()
-    render(){
-      return(
-          <div class="container">
-            <div class="item">
-            <button>Add Reminder</button>
-            <button>Add Reminder</button>
+    render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className="wrapper">
+              <h1>Reminder App</h1>
+                             
+            </div>
+        </header>
+        <div className='container'>
+          <section className='add-item'>
+                <form onSubmit={this.handleSubmit}>
+                  Here is my reminder app that doesn't actually remind you of anything. Created using Fiebase and React.js on stackblitz.com. `
+                </form>
+          </section>
         </div>
-    </div>
-      )
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
