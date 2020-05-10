cdedd57131bb9d025984239a47d7d39f07964155
Mooffyman13 <n.a.smith@student.saintkentigern.com>
2020-05-11 09:19:37 +1200

    1 file modified

--- components/navigation.js
+++ components/navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
        <div className="navbar">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/reminderinput">Create A Reminder</NavLink>
-          <NavLink to="/stuff">How this works</NavLink>
+          <NavLink to="/stuff">Relevant Implications</NavLink>
        </div>
     );
 }
